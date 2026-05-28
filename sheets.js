70dd0ce984053f72629d04c7faa5609403e18e9c
Amin <42432595+aminsunny@users.noreply.github.com>
2026-05-28 09:07:02 +0330

    Update Google Sheets configuration credentials

--- sheets.js
+++ sheets.js
@@ -5,11 +5,11 @@
 
 const SHEETS_CONFIG = {
     // از فایل JSON که دانلود کردی:
-    client_email: "aminshop-bot@aminshop-497507.iam.gserviceaccount.com",
-    private_key: "",
+    client_email: "amin-manager@aminshop-497507.iam.gserviceaccount.com",
+    private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDIeGP0VpwJ2pEB\n6wkj6C+xYYQZR4ee1Z5TC0KiLZoAxps9oCZdPN0TxqWEbhUHNoblrivJAZwSZtKA\nT7RBAljdN3vt2Ln4MWbEQATmJ3E89Udd8cJPEc8layEiyltgxLZDM2bjVlDO0yHX\nm7+y6GqXsjHpJIdYJPwci8q6Kod2AFTBY/armNPhI0ggWJmsqDVrEBr9l9TtCwt2\n9KH0rKiaVb3S4gN/QGe/+NKKxIZ+ipe3vNoqyJRWfnlUP2rAwmdEsjodiON8G+4C\nTfEjyeZUyCiRzUNwTKB46lGOjPVRI7qkipEjowu36djguLf+O3DtP3P9MaScVq25\noRr1/CP5AgMBAAECggEABalGs1f9zJZwSWRUi//K1wDIvPkE5rKkJ7a18UQF6Rcy\nu50J8FCfcqdFtdVxARTkXtiaEnFZ8bgXIjn9/mb9UPP36znXWJ+2hfzD1qcO/thz\nZT8GTHrVhXo+w4+A4Jz1g7TQJ0PhANI8Dq1ujrQbDIEjATE/pDjZKMo21aaNUEL5\nw1iOQ8SK8efVJK8lDYTTVb2FjSSujiJO2cjvU1uDALHwqkQRSuR2U2yn9cd2J/6v\neFvR166bHgRRtFJ45BIV9vIbVLsicwZK7dm26M/CEBgh+wMeLH40Ym1Bi0gtisJx\n93+Hp12khs6zjev61HcOfnuuYwxGl0s9mCZwHbaXQQKBgQD8M+T5cG/qzyNF2gUr\neMetPzpwVuKQuPL0t8dVEB09rKtBoV9w7hxwtb/N8CwFz74Vsdfwq+1Z+UUkJHxE\nhL6ptY0gkMzaQ9NgmvrhHJLx4lceSSBlMCc3TONMIPQ2p7aFS2FyNxj7NW+VPpbP\ngkP6A7YVA2Go/h7Re0M3PPcwWQKBgQDLfRhoa7P2zlJxNPYQkgWxEik9+elIYO3v\n7z0xMBtQwcGsZ6GMc6SBYL7uEPnSQ7Cs5Zclql2ounZEiKUADTIbruFImg2EekOl\n+HlOr0DLCjklyjwtzh3AOkouQzRqkg1I/3WF2abijxpAx9rp+pTzOVMHP29qKZnp\nZjvTT4UcoQKBgEWxGW8McQDT6I1e59rRb2wicsWkXMtdMFYLituorkvisRhbvYH5\naoaC0tPXsHKSq5ZXJLLu75HTXhXUtzo/7Gf9MVh0awdNgtXJPdaDDOAmiahm0Lrl\niZCwgIcC/Dk2Myu2XuEu9IpGg+Ub/JSvDJXD/MblvlDymBWPGE3lXPFRAoGAOntK\nT4BcqVrkoLp62YpN7nTsu24emHmbCHD2YjOUNiJpWfYynJ4DslcuqFbipYZkuVwG\nk4GiB0MHiGzJFvmz4/bfsjDsH53P4VxG6NE/Sts2T2EO6I8rR7q45RDVeKdfABLD\nIOPLraM5BVMq3EHzGu/np9aYW6d3H3gNS464D+ECgYBfJe93kNGGO3L5LmGCEPPp\nYvWn1XmrwfY/5QCCTMNiLBCGyURV/fzxvTQAWa8rlSGv3xrHk91HkDIsFRUcu5cg\njBRRoFJvBDugdBJj81OZ7iJqgjXM/igyUccJx6NbvFqvBxwdJSk0yhM09C1gg7QJ\nntI+28x8sKGsS38tV5y2qQ==\n-----END PRIVATE KEY-----\n",
 
     // ID شیت گوگل (از آدرس مرورگر):
-    spreadsheet_id: "",
+    spreadsheet_id: "1bLANucgiYAlQWfzwoKys5Cq9PDBYXqjuZfHK0FyoBwU",
 };
 
 // نام شیت‌ها
